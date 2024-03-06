30512c37fb47d0a928e33b709589877d1250e11f
Ale <duartemattias4@gmail.com>
2024-03-05 22:09:06 -0400

    end auth module

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\AuthLoginRequest;
+use App\Http\Requests\Auth\AuthRegisterRequest;
+use App\Http\Resources\Auth\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login( AuthLoginRequest $request )
+    {
+        $credentials = $request->validated();
+
+        if( !Auth::attempt($credentials) )
+        {
+            return response()->json([
+                "message" => "Correo o contraseña incorrectos"
+            ], 422);
+        }
+
+
+        $user = User::find( Auth::user()['id'] );
+        $token = $user->createToken('token')->plainTextToken;
+        
+        return response()->json([
+            'user' => new UserResource($user),
+            'token' => $token
+        ]);
+    }
+
+    public function register( AuthRegisterRequest $request )
+    {
+        $credentials = $request->validated();
+        $user = User::create($credentials);
+
+        return response()->json([
+            "message" => "Usuario registrado con exito",
+            "user" => new UserResource($user),
+        ], 201);
+
+    }
+
+    public function logout( Request $request )
+    {
+        $user = $request->user();   
+        $user->currentAccessToken()->delete();
+
+        return response()->json([
+            "message" => "La sesion se cerro correctamente",
+        ]);
+
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Category\CategoryController;
 use App\Http\Controllers\Product\ProductController;
 use App\Http\Controllers\Sale\SaleController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,21 +17,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->group(function () {
 
 
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::post('/categories', [CategoryController::class, 'store']);
-Route::get('/categories/{term}', [CategoryController::class, 'show']);
-Route::put('/categories/{id}', [CategoryController::class, 'update']);
-Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
-
-
-Route::apiResource('/products', ProductController::class);
-
-
-Route::apiResource('/sales', SaleController::class)
+    Route::post('/auth/logout', [ AuthController::class, 'logout']);
+    
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::get('/categories/{term}', [CategoryController::class, 'show']);
+    Route::put('/categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+    
+    
+    Route::apiResource('/products', ProductController::class);
+    
+    Route::apiResource('/sales', SaleController::class)
     ->only(['index','store', 'show']);
     // ->except(['update','destroy']);
+    
+    Route::post('/auth/register', [ AuthController::class, 'register']);
+
+
+});
+
+
+
+
+Route::post('/auth/login', [ AuthController::class, 'login']);
